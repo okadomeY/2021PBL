ca8ce8e0adc8bfb58418f6a97d6a64305e0e9ad7
hj2036 <a2036hh@aiit.ac.jp>
2021-09-10 09:23:11 +0000

    chart version

--- mysite/moticom/views.py
+++ mysite/moticom/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 
-class TopView(generic.TemplateView):
+from .models import Report
+
+class TopView(generic.ListView):
     template_name = 'moticom/index.html'
-
+    context_object_name = 'latest_report_list'
+    
+    def get_queryset(self):
+        return Report.objects.filter(
+            created_at__lte=timezone.now()
+            ).order_by('-created_at')[:10]
 
 class BoardView(generic.TemplateView):
     template_name = 'moticom/board.html'
