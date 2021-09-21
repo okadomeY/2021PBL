0f4f5cda408492c9ea4f9fdf3dc7ac8e24bb1510
hj2036 <a2036hh@aiit.ac.jp>
2021-09-21 13:22:13 +0000

    各ページの編集完了＋管理者用ページ複数作成

--- mysite/moticom/urls.py
+++ mysite/moticom/urls.py
@@ -14,4 +14,11 @@ urlpatterns = [
     path('complaints', views.ComplaintsView.as_view(), name='complaints'),
     path('help', views.HelpView.as_view(), name='help'),
     path('admin', views.AdminView.as_view(), name='admin'),
+    path('analysis', views.AnalysisView.as_view(), name='analysis'),
+    path('user', views.UserView.as_view(), name='user'),
+    path('layout', views.LayoutView.as_view(), name='layout'),
+    path('genre_manage', views.Genre_ManageView.as_view(), name='genre_manage'),
+    path('filter', views.FilterView.as_view(), name='filter'),
+    path('sorting', views.SortingView.as_view(), name='sorting'),
+    path('linking', views.LinkingView.as_view(), name='linking'),
     ]
--- mysite/moticom/views.py
+++ mysite/moticom/views.py
@@ -42,3 +42,24 @@ class HelpView(generic.TemplateView):
     
 class AdminView(generic.TemplateView):
     template_name = 'moticom/admin.html'
+    
+class AnalysisView(generic.TemplateView):
+    template_name = 'moticom/analysis.html'
+    
+class UserView(generic.TemplateView):
+    template_name = 'moticom/user.html'
+
+class LayoutView(generic.TemplateView):
+    template_name = 'moticom/layout.html'
+    
+class Genre_ManageView(generic.TemplateView):
+    template_name = 'moticom/genre_manage.html'
+    
+class FilterView(generic.TemplateView):
+    template_name = 'moticom/filter.html'
+    
+class SortingView(generic.TemplateView):
+    template_name = 'moticom/sorting.html'
+    
+class LinkingView(generic.TemplateView):
+    template_name = 'moticom/linking.html'
